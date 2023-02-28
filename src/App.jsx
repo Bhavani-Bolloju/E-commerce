d3441a917c5d064e1131f22a386e329e684387a2
Ruth Ramya <ramya.b228@gmail.com>
2023-02-28 17:21:23 +0530

    created header-nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
 import React, { useEffect, useState } from "react";
+import Header from "./components/header/Header";
 
 function App() {
-  // const [data, setData] = useState(null);
-
-  // useEffect(() => {
-  //   const fakeData = async function () {
-  //     const res = await fetch(
-  //       "https://dummyjson.com/products/category/groceries"
-  //     );
-
-  //     const data = await res.json();
-  //     setData(data.products);
-  //   };
-
-  //   fakeData();
-  // }, []);
-
-  // console.log(data);
-
   return (
     <div>
-      {/* {data &&
-        data.map((product) => (
-          <div>
-            <p>{product.title}</p>
-            {product.images.map((item) => (
-              <img src={item} />
-            ))}
-          </div>
-        ))} */}
+      <Header />
     </div>
   );
 }
 
 export default App;
+
+// const [data, setData] = useState(null);
+
+// useEffect(() => {
+//   const fakeData = async function () {
+//     const res = await fetch(
+//       "https://dummyjson.com/products/category/groceries"
+//     );
+
+//     const data = await res.json();
+//     setData(data.products);
+//   };
+
+//   fakeData();
+// }, []);
+
+// console.log(data);
