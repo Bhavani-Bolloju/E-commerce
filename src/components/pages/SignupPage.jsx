0f2ecbed4fe918ebe4f0063cb0037f4585e874fa
Ruth Ramya <ramya.b228@gmail.com>
2023-03-07 21:04:45 +0530

    implemented singin functionality

--- src/components/pages/SignupPage.jsx
+++ src/components/pages/SignupPage.jsx
@@ -1,13 +1,73 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./Singup.module.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 function SignupPage() {
+  const fullnameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const submitHandler = async function (e) {
+    e.preventDefault();
+    const fullname = fullnameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    try {
+      console.log(fullname, email, password);
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const user = res.user;
+      if (!res) throw new Error("user already exists");
+      const addData = await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        fullname: fullname,
+      });
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+
+    fullnameRef.current.value = "";
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+  };
+
   return (
     <div className={classes.signup}>
-      <form>
-        <input type="text" placeholder="fullname" />
-        <input type="email" placeholder="Email" />
-        <input type="test" placeholder="password" />
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="fullname"
+          ref={fullnameRef}
+          onFocus={() => {
+            setError(false);
+          }}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          ref={emailRef}
+          onFocus={() => {
+            setError(false);
+          }}
+        />
+        <input
+          type="test"
+          placeholder="password"
+          ref={passwordRef}
+          onFocus={() => {
+            setError(false);
+          }}
+        />
+        {error && <p className={classes.error}>{error}</p>}
         <button>Sign Up</button>
       </form>
     </div>
