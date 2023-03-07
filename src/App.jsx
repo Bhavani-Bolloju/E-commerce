712f975ee1109499f4553b72fdfb319372d9039f
Ruth Ramya <ramya.b228@gmail.com>
2023-03-07 21:24:25 +0530

    completed sign in functionality

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { useSelector } from "react-redux";
 import CartPage from "./components/pages/CartPage";
 import Cart from "./components/header/Cart";
 import LoginPage from "./components/pages/LoginPage";
-import SignupPage from "./components/pages/SignupPage";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -22,7 +21,6 @@ function App() {
           <Route path="cartItems" element={<Cart />}></Route>
           <Route path="savedItems" element={<SavedItems />}></Route>
         </Route>
-        <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
       </Routes>
     </div>
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import classes from "./Login.module.scss";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth, db } from "../firebase/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
-function LoginPage() {
-  return <div>LoginPage</div>;
+function SignupPage() {
+  const fullnameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [error, setError] = useState(null);
+  const [signedIn, setSignedIn] = useState(false);
+
+  const navigate = useNavigate();
+
+  const submitHandler = async function (e) {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (signedIn) {
+      try {
+        const loginUser = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        if (!loginUser) throw new Error("failed to login");
+        navigate("/");
+      } catch (error) {
+        setError(error.message);
+      }
+    } else {
+      const fullname = fullnameRef.current.value;
+      try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        if (!res) throw new Error("user already exists");
+        const addData = await addDoc(collection(db, "users"), {
+          uid: user.uid,
+          fullname: fullname,
+        });
+        fullnameRef.current.value = "";
+        navigate("/");
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      }
+    }
+
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+  };
+
+  return (
+    <div className={classes.signup}>
+      <form onSubmit={submitHandler}>
+        {!signedIn && (
+          <input
+            type="text"
+            placeholder="fullname"
+            ref={fullnameRef}
+            onFocus={() => {
+              setError(false);
+            }}
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Email"
+          ref={emailRef}
+          onFocus={() => {
+            setError(false);
+          }}
+        />
+        <input
+          type="test"
+          placeholder="password"
+          ref={passwordRef}
+          onFocus={() => {
+            setError(false);
+          }}
+        />
+        {error && <p className={classes.error}>{error}</p>}
+        <button>{signedIn ? "Log In" : "Sign Up"}</button>
+        <p className={classes.switch}>
+          Already have na Account?{" "}
+          <span onClick={() => setSignedIn((prev) => !prev)}>
+            {signedIn ? "sign up" : "Login"}
+          </span>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default LoginPage;
+export default SignupPage;
